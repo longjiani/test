fea2400f3780a768026d04d71711a089e2132f56
jiani <jiani@pc-longjiani>
2015-01-22 11:01:43 +0100

    commit 1 de jiani

--- TestGit/src/testgit/TestGit.java
+++ TestGit/src/testgit/TestGit.java
@@ -16,5 +16,6 @@ public class TestGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("cynthia");
+        System.out.println("jiani");
     }
 }
